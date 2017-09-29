a272582242a2026f2626428e46c29f1c223ace2e
Kenneth <kennega@stud.ntnu.no>
2017-09-29 04:25:57 +0200

    project euler exercise 1

---
+++ multipleof.py
@@ -0,0 +1,16 @@
+
+
+def multiple_of(n):
+    sum = 0
+
+    for i in range(n):
+
+        if i % 3 == 0 and i % 5 == 0:
+            sum+= i
+
+    return sum
+
+
+thousand = multiple_of(1001)
+
+print(thousand)
